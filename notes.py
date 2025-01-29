788769111505514345a97585884c94e67f8bc4e7
jackabiz <Jackclarkwins@gmail.com>
2025-01-29 11:50:34 -0700

    hola

---
+++ notes.py
@@ -0,0 +1,11 @@
+#  Jack Clark, Variable notes
+
+greeting = "hey, you!"
+
+print(greeting) 
+
+greeting = input("what is your name? ")
+
+print("hello",greeting,"!","I'm bartholemul! Do you know why we're in a basement?")\
+
+input("yes" or "no")
---
+++ variables/practice.py
@@ -0,0 +1,21 @@
+# Jackson Clark, variable practice
+
+username = input("what is your name?")
+
+schoolname = input ("what school do you go to?")
+
+smallnum = input("name a number between 1 and 100")
+
+bignum = input("name a number between 100 and 1000")
+
+breakfast = input("what did you have for breakfast today?")
+
+C = input("what is your favorite color?")
+
+eyeC = input("what is your eye color?")
+
+yr = input("what year is it?")
+
+age = input("how old are you?")
+
+favsub = input("what is your favorite academic subject?")
