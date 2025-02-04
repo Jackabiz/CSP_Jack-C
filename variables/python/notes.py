d46f4a61f1a0209d54f8333f14fab65cd989b0d7
Jackabiz <Jackclarkwins@gmail.com>
2025-02-04 19:17:45 +0000

    Text adventure!

--- variables/python/notes.py
+++ variables/python/notes.py
@@ -1,16 +1,18 @@
 #  Jack Clark, Variable notes
 
-print("hey, you!") 
+print("'hey, you!'") 
 
-name = input("what is your name? ")
+name = input("'what is your name?' ")
 
-print(f"hello {name}!","I'm bartholemul! Do you know why we're in a basement?")
+print(f"'hello {name}!","I'm bartholemul! Do you know why we're in a basement?'")
 
 response = input("(yes or no)")
 
 positive = "yes" or "Yes"
 
 if response==positive:
- print("oh, that's amazing!")
+ print("'oh, that's amazing!'")
 else:
- print("why are you even here then?")
+ print("'you're no use to be then.' \n you are dead")
+ deathscreen = input("try again? (yes or no)")
+ 
