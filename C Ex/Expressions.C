bca1865952c72c83c9b0cab543304557e80a4e55
jackabiz <Jackclarkwins@gmail.com>
2025-02-04 11:22:23 -0700

    eeeeee

--- C Ex/Expressions.C
+++ C Ex/Expressions.C
@@ -7,6 +7,8 @@ int main (void){
 
 //exponents are represented by pow(#,#) and has to be stored in a seperate variable
 
+// .#f is how you round things
+
 //C:\msys64\ucrt64\bin is how you set up C
 
 printf("%f\n", equation);
