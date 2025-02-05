c7ef09b046c62a1703ec73267df9047f46fbd4df
Jackabiz <Jackclarkwins@gmail.com>
2025-02-04 21:34:00 -0700

    progress2

--- variables/python/notes.py
+++ variables/python/notes.py
@@ -1,26 +1,31 @@
 #  Jack Clark, Variable notes
 
+greeting = "'hey, you!'"
+
 playing = "true"
 
+positive = "Yes" or "yes" 
+
+negative = "No" or "no"
+
 while playing=="true":
-  print("'hey, you!'")
-  
-name = input("'what is your name?' ")
-
-print(f"'hello {name}!","I'm bartholemul! Do you know why we're in a basement?'")
-
-response = input("(yes or no)")
-
-positive = "yes" or "Yes"
-
-negative = "no" or "yes"
-
-if response==positive:
- print("'well, I suppose that makes sense... let's see if we can get out of here, shall we?'")
+  print(greeting)
+  greeting = ""
+  name = input("'what is your name?' ")
+  print(f"'hello {name}!","I'm bartholemul! Do you know why we're in a basement?'")
+  response = input("")
+  if response==negative:
+    print("'well, I suppose that makes sense... let's see if we can get out of here, shall we?'")
+  if response==positive:
+    print("'you must be one of my captors! die!' \n \n you are dead")
+    deathscreen = input("try again? (yes or no)")
+    if deathscreen==negative:
+     playing = "false"
 else:
- print("'you must be one of my captors! die!' \n \n you are dead")
- deathscreen = input("try again? (yes or no)")
- if deathscreen==negative:
-    playing = "false"
+  print("thanks for playing!")
+     
+     
+
+  
 
 
