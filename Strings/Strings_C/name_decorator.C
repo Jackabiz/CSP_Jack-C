9835fc9346908a51befd6b670fde98a759ea57cc
Jackabiz <Jackclarkwins@gmail.com>
2025-02-13 12:46:19 -0700

    eafreaq

--- Strings/Strings_C/name_decorator.C
+++ Strings/Strings_C/name_decorator.C
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (void){
     char name[30];
-    printf("what is your name?");
+    printf("what is your name?\n");
     scanf("%s", name);
-    printf("<<< %s >>>", name);
+    char name1[] = "<<<";
+    char decor1_b[] = ">>>";
+    strcat(name1, name);
+    strcat(name1, decor1_b);
+    printf("%s", name1);
 
     return 0;
 }
