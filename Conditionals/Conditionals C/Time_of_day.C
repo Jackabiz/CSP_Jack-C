c69c52b989cc898ac39f8794ac12f10feaf597e4
jackabiz <Jackclarkwins@gmail.com>
2025-03-06 11:12:50 -0700

    e

--- Conditionals/Conditionals C/Time_of_day.C
+++ Conditionals/Conditionals C/Time_of_day.C
@@ -1,2 +1,26 @@
 //Jackson Clark, Time of Day C
 
+#include <stdio.h>
+
+int main (void){
+   
+    int time_of_day;
+    printf("what time is it? use negatives to indicate AM and round to the nearest hour");
+    scanf("%d", &time_of_day);
+
+
+
+    if (time_of_day <= -5 && time_of_day >=-12){
+     printf("good morning!");
+    }
+    else if (time_of_day >= 1 && time_of_day <6){      
+        printf("good afternoon!");
+    }
+    else if (time_of_day >=6 && time_of_day <12){
+     printf("good evening!");
+    }
+    else if (time_of_day <= -1 && time_of_day >-5){
+        printf("what are you doing up at this hour?");
+    }
+    return 0;
+}
